31a30e922154edf6e81068b64f28edd5ae210c13
DaShaun <dashaun@dashaun.com>
2021-01-31 04:29:55 -0600

    add count.index to acre

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,9 @@
 locals {
   redis_geek_config = {
-    acre_name = var.acre_name
     acre_resource_group = var.acre_resource_group
     location = var.location
-    output_content = azurerm_resource_group_template_deployment.acre.output_content
-    acre_resource_id = azurerm_resource_group_template_deployment.acre.id
+    output_content = azurerm_resource_group_template_deployment.acre[count.index].output_content
+    acre_resource_id = azurerm_resource_group_template_deployment.acre[count.index].id
   }
 }
 
